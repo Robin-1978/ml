1e471a82848628acef8e42eb212da13c1a609225
guyiming <guyiming@gs-robot.com>
2022-09-01 14:03:15 +0800

    UU

--- include/organism.h
+++ include/organism.h
@@ -9,14 +9,14 @@
 namespace org
 {
 
-    template <typename T, T precision =  0.000001>
+    template <typename T, T precision = 0.000001>
     bool Is(const T &a, const T &b)
     {
-        auto v = a-b;
-        return v < -precision  || v > precision ? false : true;
+        auto v = a - b;
+        return v < -precision || v > precision ? false : true;
     }
 
-    struct Position : Polar
+    struct Position : Pos
     {
         Position()
         {
@@ -25,15 +25,15 @@ namespace org
 
         virtual void Random()
         {
-            rho = Random::Instance().RealInRange(0.0, 1.0);
-            theta = Random::Instance().RealInRange(M_PI, -M_PI);
+            x = Random::Instance().RealInRange(-1.0, 1.0);
+            y = Random::Instance().RealInRange(-1.0, 1.0);
         }
 
-        bool operator ==(const Position& p) constexpr
+        bool operator==(const Position &p) const
         {
-            auto dRho = rho - p.rho;
-            auto dTheta = theta - p.theta;
-            if((dRho >= -precision && dRho <= precision) || (dTheta >= -precision && dTheta <= precision))
+            auto dx = x - p.x;
+            auto dy = y - p.y;
+            if ((dx >= -precision && dx <= precision) || (dy >= -precision && dy <= precision))
             {
                 return true;
             }
@@ -41,16 +41,35 @@ namespace org
         }
     };
 
-    struct State : Polar
+    struct State : Position
     {
-        double yaw;
-        double steering, speed;
+        double yaw, speed;
+
+        void Step(double v , double w)
+        {
+            auto dx = speed * std::cos(yaw);
+            auto dy = speed * std::sin(yaw);
+
+            speed += v;
+
+            if(speed < -1.0) speed = -1.0;
+            if(speed > 1.0) speed = 1.0;
+
+            yaw += w;
+            if (yaw <= -M_PI)
+                yaw += 2 * M_PI;
+            if (yaw > M_PI)
+                yaw -= 2 * M_PI;
+
+            dxx =  speed * std::cos(yaw);
+            dyy += speed * std::sin(yaw);
+            x += (dx + dxx)/2;
+            y += (dy + dyy)/2;
+        }
     };
 
     struct Food : Position
     {
-
-
     };
 
     class Organism : State
@@ -64,29 +83,28 @@ namespace org
 
         void Step()
         {
-
         }
 
-        Polar Decide(const std::vector<Food>& foods, const std::vector<Organism>& organizations)
+        Polar Decide(const std::vector<Food> &foods, const std::vector<Organism> &organizations)
         {
             values v;
-            for(auto& f : foods)
+            for (auto &f : foods)
             {
                 auto delta = f - *this;
-                v.push_back(delta.rho);
-                v.push_back(delta.theta);
+                v.push_back(delta.x);
+                v.push_back(delta.y);
             }
-            for(auto p : organizations)
+            for (auto p : organizations)
             {
-                auto delta = p -*this;
-                v.push_back(delta.rho);
-                v.push_back(delta.theta);
+                auto delta = p - *this;
+                v.push_back(delta.x);
+                v.push_back(delta.y);
             }
             auto decision = _brain(v);
-            speed += decision[0];
-            steering += decision[1];
+            Step(decision[0], decision[1])
             // TODO:: Varify boundary
         }
+
     private:
         unsigned _score;
         org::Network _brain;
@@ -95,45 +113,45 @@ namespace org
     class World
     {
     public:
-        World(unsigned oc, unsigned fc)
-            :_organisms(oc), _apples(fc)
+        World(unsigned oc, unsigned fc, double ratio = 1000)
+            : _organisms(oc), _apples(fc)
         {
-
         }
 
         void Step()
         {
-            for(auto& o : _organisms)
+            for (auto &o : _organisms)
             {
                 auto apples = GetApples(o);
-                auto organizations = GetOrganizations(o);
+                auto organizations = GetOrganisms(o);
                 o.Decide(apples, organizations);
             }
 
-            for(auto& o : _organisms)
+            for (auto &o : _organisms)
             {
-                if(o.steering < -2 * M_PI / 3)
+                o.yaw += o.steering / 4;
+                if (p.yaw <= -M_PI)
+                    p.yaw += 2 * M_PI;
+                if (p.yaw > M_PI)
+                    p.yaw -= 2 * M_PI;
 
                 o.rho += o.speed;
-                if(o.rho > 1.0) o.rho -= 1.0;
-                if(o.rho < -1.0) o.rho = -1.0;
+                if (o.rho > 1.0)
+                    o.rho -= 1.0;
+                if (o.rho < -1.0)
+                    o.rho = -1.0;
 
                 o.theta += o.steering;
-
             }
-            
         }
 
-        std::vector<Food> GetApples(const Organism& o)
+        std::vector<Food> GetApples(const Organism &o)
         {
-
         }
 
-        std::vector<Food> GetOrganism(const Organism& o)
+        std::vector<Food> GetOrganisms(const Organism &o)
         {
-
         }
-
 
     private:
         std::vector<Organism> _organisms;
